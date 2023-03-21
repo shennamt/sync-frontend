834421842925a422ddf210537e43ae0b9a6e3214
aaronyeo007 <115003546+aaronyeo007@users.noreply.github.com>
2023-03-21 11:23:56 +0800

    Revert "Xf01"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { useState } from "react";
-import NewForm from "./NewForm";
+import logo from './logo.svg';
+import './App.css';
 
-const App=()=>{
-
-
-
-
-
-return(
-<>
-    <h1>Holidays! Celebrate!</h1>
-    <NewForm />
-</>
-
-)
-
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
