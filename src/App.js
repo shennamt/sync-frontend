01ea8387391fafa47cf63ef3b3e5126d67cbf598
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-21 13:07:48 +0800

    chore: removed CRA template items

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { useState } from "react";
 import NewForm from "./NewForm";
 
-const App=()=>{
-
-
-
-
-
-return(
-<>
-    <h1>Holidays! Celebrate!</h1>
-    <NewForm />
-</>
-
-)
-
+const App = () => {
+  return (
+    <div className ="App">
+        <h1>S Y N C</h1>
+        <NewForm />
+    </div>
+  )
 }
 
 export default App;
