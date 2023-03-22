e1f170801a730a9e0de860d412837aced5a59192
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-22 12:50:48 +0800

    feat: Dashboard content component created

---
+++ src/components/BoardContent/BoardContent.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './BoardContent.scss';
+
+const BoardContent = () => {
+  return (
+    <div className="BoardContent">
+      <div className="Column">
+        <header> Project One </header>
+        <ul>
+          <li> 
+            <img src="https://images.unsplash.com/photo-1565008447742-97f6f38c985c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80"
+            alt="building construction"/>
+            Title: Project Name
+          </li>
+          <li> Add tasks below </li>
+          <li> Add tasks below </li>
+          <li> Add tasks below </li>
+          <li> Add tasks below </li>
+        </ul>
+        <footer>Add another task</footer>
+      </div>
+
+      <div className="Column">
+        <header> Project One </header>
+        <ul>
+          <li> 
+            <img src="https://images.unsplash.com/photo-1565008447742-97f6f38c985c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80"
+            alt="building construction"/>
+            Title: Project Name
+          </li>
+          <li> Add tasks below </li>
+          <li> Add tasks below </li>
+          <li> Add tasks below </li>
+          <li> Add tasks below </li>
+        </ul>
+        <footer>Add another task</footer>
+      </div>
+    </div>
+  )
+}
+
+export default BoardContent;
