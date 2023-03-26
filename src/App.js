7f35d96c0338b5e3d55b19cdac9df020b2323e08
xiangfengg <yongxiangfeng13@gmail.com>
2023-03-26 17:43:49 +0800

    Auth for front end

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React, { useState } from 'react';
-import NewForm from './NewForm';
-import './App.scss'
-import Dashboard from './Dashboard';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+
+import "./App.scss";
+
+import Dashboard from "./pages/Dashboard";
+
+import Login from "pages/Login";
+import Signup from "pages/Signup";
+import NavBarApp from "components/NavBarApp";
 
 const App = () => {
+  const { user } = useAuthContext();
   return (
     <div className="App">
+      <BrowserRouter>
         {/* <h1 className="SyncH1">S Y N C</h1> */}
-        < Dashboard />
-        {/* <NewForm /> */}
+        <NavBarApp />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              // user is true render Dashboard else navigate to login
+              element={user ? <Dashboard /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Dashboard.scss";
+
+// Custom Components
+import NavBarApp from "components/NavBarApp";
+import NavBarBoard from "components/NavBarBoard/NavBarBoard";
+import BoardContent from "components/BoardContent/BoardContent";
+
+const Dashboard = () => {
+  return (
+    <div className="Dashboard">
+      <NavBarApp />
+      <NavBarBoard />
+      <BoardContent />
+    </div>
+  );
+};
+
+export default Dashboard;
