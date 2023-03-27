684fa7d631bd3b3a05d8812d3b13f77a2146f6e2
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-27 18:18:25 +0800

    feat: restyled register page

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useSignup } from 'hooks/useSignup';
 import './RegisterPage.css'
-import { TextField, FormControlLabel, Checkbox, Button, Link, Radio} from '@mui/material'
+import {
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Radio
+} from '@mui/material'
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 
 const RegisterPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signup, error, isLoading } = useSignup();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await signup(email, password);
+  };
 
   return (
     <div className='register'>
@@ -14,26 +32,45 @@ const RegisterPage = () => {
         <p className='body__text'>Create your account and Start Your Next Collaboration.</p>
       </div>
 
-      <div className='register__body'>
-        <div className='input__field'>
+      <form className='register__body' onSubmit={handleSubmit}>
+        {/* <div className='input__field'>
           <TextField label="First Name" variant="outlined" size="small" placeholder="e.g Jane" fullWidth required/>
         </div>
 
         <div className='input__field'>
           <TextField label="Last Name" variant="outlined" size="small" placeholder="e.g. Doe" fullWidth required/>
+        </div> */}
+
+        <div className='input__field'>
+          <TextField
+            label="Email"
+            variant="outlined"
+            size="small"
+            placeholder="e.g. xyz@gmail.com"
+            fullWidth
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
         </div>
 
         <div className='input__field'>
-          <TextField label="Email" variant="outlined" size="small" placeholder="e.g. xyz@gmail.com" fullWidth required/>
+          <TextField
+            label="Password"
+            variant="outlined"
+            size="small"
+            type="password"
+            placeholder="Enter your password"
+            fullWidth
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
         </div>
 
-        <div className='input__field'>
-          <TextField label="Password" variant="outlined" size="small" type="password" placeholder="Enter your password" fullWidth required/>
-        </div>
-
-        <div className='input__field'>
+        {/* <div className='input__field'>
           <TextField label="Confirm Password" variant="outlined" size="small" type="password" placeholder="Re-enter your password" fullWidth required/>
-        </div>
+        </div> */}
 
         <div className='input__field'>
           <FormControl className='radio__occupation'>
@@ -52,13 +89,18 @@ const RegisterPage = () => {
 
         <FormControlLabel control={<Checkbox defaultChecked />} label="I accept the terms and conditions." />
         <br />
+
         <div className='button'>
-          <Button variant="contained" fullWidth>Login</Button>
+          <Button disabled={isLoading} variant="contained" fullWidth>Login</Button>
         </div>
-        <div className='login__link'>
-          <p>Already have an account? <Link>Login</Link></p>
-        </div>
+
+        {error && <div className="error">{error}</div>}
+      </form>
+
+      <div className='login__link'>
+          <p>Already have an account? <Link to="/">Login</Link></p>
       </div>
+
     </div>
   )
 }
