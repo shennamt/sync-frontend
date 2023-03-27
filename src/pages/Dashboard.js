4b09f6165f53d8f148cc4a269323416c2bb4b0cc
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-28 01:17:06 +0800

    chore: nav bar funcitons

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,15 +4,14 @@ import "./Dashboard.scss";
 
 // Custom Components
 import DashboardNav from "components/DashboardNav/DashboardNav";
-// import NavBarBoard from "components/NavBarBoard/NavBarBoard";
-// import BoardContent from "components/BoardContent/BoardContent";
+import BoardContent from "components/BoardContent/BoardContent";
+
 
 const Dashboard = () => {
   return (
     <div className="Dashboard">
       <DashboardNav/>
-      {/* <NavBarBoard /> */}
-      {/* <BoardContent /> */}
+      <BoardContent />
     </div>
   );
 };
