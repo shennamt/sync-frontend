d76cdd0951c75ac95f390d70cfbcb2701645c0f9
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-29 12:29:57 +0800

    Add: logout functi on, favourites and private options as well but the latter 2 ar e not functional yet

--- src/components/CommonUse/SideNav.js
+++ src/components/CommonUse/SideNav.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Drawer, List } from '@mui/material'
+
+// import { useSelector, useDispatch } from 'react-redux'
+// import { useNavigate } from 'react-router-dom'
+import { useAuthContext } from "hooks/useAuthContext";
+import { useLogout } from 'hooks/useLogout';
+
+import { Box, Drawer, List, ListItem, Typography, IconButton } from '@mui/material'
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined' 
+
 
 const SideNav = () => {
-  const user = useSelector((state) => state.user.value)
+  // const user = useSelector((state) => state.user.value)
+  // const navigate = useNavigate();
+  // const logout = () => {
+  //   localStorage.removeItem('token');
+  //   Navigate('/login')
+  // }
+
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
   const sidebarWidth = 250
+
+  const handleClick = () => {
+    logout();
+  };
 
   return (
     <Drawer
@@ -23,12 +43,54 @@ const SideNav = () => {
         width: sidebarWidth,
         height: '100vh',
         backgroundColor: '#1976d2',
-      }}
-      >
-
+      }}>
+        <ListItem>
+          <Box sx={{
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}>
+            <Typography variant='body2' fontWeight='700' style={{ color: 'white' }}>
+              {user.email}
+            </Typography>
+            <IconButton>
+              <LogoutOutlinedIcon fontSize='small' onClick={handleClick} style={{ color: 'white' }}/>
+            </IconButton>
+          </Box>
+        </ListItem>
+        <Box sx={{ paddingTop: '10px'}}/>
+        <ListItem>
+          <Box sx={{
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}>
+            <Typography variant='body2' fontWeight='700' style={{ color: 'white' }}>
+              Favourites
+            </Typography>
+          </Box>
+        </ListItem>
+        <Box sx={{ paddingTop: '10px'}}/>
+        <ListItem>
+          <Box sx={{
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}>
+            <Typography variant='body2' fontWeight='700' style={{ color: 'white' }}>
+              Private
+            </Typography>
+            <IconButton>
+              <AddBoxOutlinedIcon fontSize='small' style={{ color: 'white' }}/>
+            </IconButton>
+          </Box>
+        </ListItem>
       </List>
     </Drawer>
   )
 }
 
-export default SideNav
+export default SideNav;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,13 @@
 import React from "react";
-
 import './HomePage.css'
+import SideNav from "components/CommonUse/SideNav";
 
 import DashboardNav from "components/DashboardNav/DashboardNav";
 
 const HomePage = () => {
   return (
     <div className="Dashboard">
-      <DashboardNav />
+      <SideNav />
     </div>
   );
 };
