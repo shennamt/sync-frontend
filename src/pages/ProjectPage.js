224f25c26a31e3185c26c2de8aaea023cb5a9006
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-29 22:31:56 +0800

    feat: set up projectPage to include sidenav

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import SideNav from "components/CommonUse/SideNav"
+import { Box } from "@mui/material"
 
 const ProjectPage = () => {
   return (
-    <div>
-      Project
+    <div className="ProjectPage" style={{ display: 'flex', alignItems: 'center' }}>
+      <Box>
+        <SideNav />
+      </Box>
+      <Box sx={{
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1
+      }}>
+        Projects
+      </Box>
     </div>
-  )
+  );
 }
 
 export default ProjectPage
