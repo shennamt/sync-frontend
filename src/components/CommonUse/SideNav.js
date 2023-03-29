b9ed398492ee6af88a903a28276eeadd55d118c6
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-29 11:33:29 +0800

    chore: added applayout in case i cant utilise the fetch authcontext

--- src/components/CommonUse/SideNav.js
+++ src/components/CommonUse/SideNav.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Drawer, List } from '@mui/material'
 
 const SideNav = () => {
+  const user = useSelector((state) => state.user.value)
+  const sidebarWidth = 250
+
   return (
-    <div>
-      SideNav
-    </div>
+    <Drawer
+    container={window.document.body}
+    variant='permanent'
+    open={true}
+    sx={{
+      width: sidebarWidth,
+      height: '100vh',
+      '& > div': { borderRight: 'none' }
+    }}
+    >
+      <List
+      disablePadding
+      sx={{
+        width: sidebarWidth,
+        height: '100vh',
+        backgroundColor: '#1976d2',
+      }}
+      >
+
+      </List>
+    </Drawer>
   )
 }
 
