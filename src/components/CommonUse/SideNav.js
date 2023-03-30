268410ec346ea8e15eb167907d2a773b28ac6f1e
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-31 03:44:25 +0800

    chore: resolve conflict on sidenav

--- src/components/CommonUse/SideNav.js
+++ src/components/CommonUse/SideNav.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import projectApi from 'api/projectApi'
-import { setProjects } from 'redux/features/projectSlice'
 
+// import { useSelector, useDispatch } from 'react-redux'
 // import { useNavigate } from 'react-router-dom'
 import { useAuthContext } from "hooks/useAuthContext"
 import { useLogout } from 'hooks/useLogout'
 
-import { Box, Drawer, List, ListItem, Typography, IconButton } from '@mui/material'
+
+import { useEffect } from 'react'
+import { setProjects } from 'redux/features/projectSlice'
+import projectApi from 'api/projectApi'
+
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  Typography,
+  IconButton
+} from '@mui/material'
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined'
 
@@ -46,71 +55,116 @@ const SideNav = () => {
     logout();
   };
 
+  // let greeting;
+  // if (user && user.occupation === "professional") {
+  //   greeting = "Hello";
+  // } else {
+  //   greeting = "Goodbye";
+  // }
+
   return (
     <Drawer
-    container={window.document.body}
-    variant='permanent'
-    open={true}
-    sx={{
-      width: sidebarWidth,
-      height: '100vh',
-      '& > div': { borderRight: 'none' }
-    }}
-    >
-      <List
-      disablePadding
+      container={window.document.body}
+      variant="permanent"
+      open={true}
       sx={{
         width: sidebarWidth,
-        height: '100vh',
-        backgroundColor: '#1976d2',
-      }}>
+        height: "100vh",
+        "& > div": { borderRight: "none" }
+      }}
+    >
+      <List
+        disablePadding
+        sx={{
+          width: sidebarWidth,
+          height: "100vh",
+          backgroundColor: "#1976d2"
+        }}
+      >
         <ListItem>
-          <Box sx={{
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-          }}>
-            <Typography variant='body2' fontWeight='700' style={{ color: 'white' }}>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between"
+            }}
+          >
+            <Typography
+              variant="body2"
+              fontWeight="700"
+              style={{ color: "white" }}
+            >
               {user.email}
             </Typography>
             <IconButton onClick={handleClick}>
-              <LogoutOutlinedIcon fontSize='small' style={{ color: 'white' }}/>
+              <LogoutOutlinedIcon fontSize="small" style={{ color: "white" }} />
             </IconButton>
           </Box>
         </ListItem>
-        <Box sx={{ paddingTop: '10px'}}/>
+        <Box sx={{ paddingTop: "10px" }} />
         <ListItem>
-          <Box sx={{
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-          }}>
-            <Typography variant='body2' fontWeight='700' style={{ color: 'white' }}>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between"
+            }}
+          >
+            <Typography
+              variant="body2"
+              fontWeight="700"
+              style={{ color: "white" }}
+            >
+              {" Mode: " + user.occupation}
+            </Typography>
+          </Box>
+        </ListItem>
+        <Box sx={{ paddingTop: "10px" }} />
+        <ListItem>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between"
+            }}
+          >
+            <Typography
+              variant="body2"
+              fontWeight="700"
+              style={{ color: "white" }}
+            >
               Favourites
             </Typography>
           </Box>
         </ListItem>
-        <Box sx={{ paddingTop: '10px'}}/>
+        <Box sx={{ paddingTop: "10px" }} />
         <ListItem>
-          <Box sx={{
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-          }}>
-            <Typography variant='body2' fontWeight='700' style={{ color: 'white' }}>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between"
+            }}
+          >
+            <Typography
+              variant="body2"
+              fontWeight="700"
+              style={{ color: "white" }}
+            >
               Private
             </Typography>
             <IconButton>
-              <AddBoxOutlinedIcon fontSize='small' style={{ color: 'white' }}/>
+              <AddBoxOutlinedIcon fontSize="small" style={{ color: "white" }} />
             </IconButton>
           </Box>
         </ListItem>
       </List>
     </Drawer>
-  )
-}
+  );
+};
 
 export default SideNav;
