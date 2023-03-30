ae8fa5f6fa81cd19faf70dc089ef061585ad4501
Shenna Mae Tan <shenmae.t@gmail.com>
2023-03-31 04:02:21 +0800

    chore: imported use Selector and dispatch oops

--- src/components/CommonUse/SideNav.js
+++ src/components/CommonUse/SideNav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-// import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 // import { useNavigate } from 'react-router-dom'
 import { useAuthContext } from "hooks/useAuthContext"
 import { useLogout } from 'hooks/useLogout'
