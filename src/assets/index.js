372727c618004b80fe9668bcd28a313770996fde
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-01 03:29:12 +0800

    feat: moved images to an assets file

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+  images: {
+    logoDark: require('./images/logo.png'),
+    appIcon: require('./images/favicon.ico')
+  },
+  colors: {
+    secondary: '#1976d2'
+  }
+}
+
+export default assets
