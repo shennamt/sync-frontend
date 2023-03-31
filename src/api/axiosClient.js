2815fd87c6001bb446c11cf8001ed5fc1eac9bc7
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-01 03:40:24 +0800

    chore: axiosClient run comments

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,27 +1,29 @@
-import axios from "axios";
-import queryString from "query-string";
+import axios from 'axios'
+import queryString from 'query-string'
 
 const baseURL = 'http://127.0.0.1:6001'
-const getToken = () => localStorage.getItem('token')
+const getToken = () => localStorage.getItem('token') // for authenticating HTTP requests
 
-const axiosClient = axios.create({
+const axiosClient = axios.create({ // creates a new Axios instance with default settings
   baseURL,
   paramsSerializer: {
-    encode: params => queryString.stringify(params)
+    encode: params => queryString.stringify(params) // serialize query parameters
   }
 })
 
+// request interceptor modifies the HTTP headers of outgoing HTTP requests
+// includes an authorization header with a bearer token.
 axiosClient.interceptors.request.use(async config => {
   return {
     ...config,
     headers: {
       'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': `Bearer ${getToken()}`
+      'authorization': `Bearer ${getToken()}`
     }
   }
 })
 
+// if the response contains data, it extracts the data property from the response.
 axiosClient.interceptors.response.use(response => {
   if (response && response.data) return response.data
   return response
@@ -32,4 +34,4 @@ axiosClient.interceptors.response.use(response => {
   throw err.response
 })
 
-export default axiosClient;
+export default axiosClient
