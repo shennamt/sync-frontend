ebe4f14cbfd1c377aa3f42d5bbcb104d4bde4b99
aaronyeo007 <aaron.yeo@icloud.com>
2023-03-31 18:03:23 +0800

    bugfix: - fix renaming of key inside res object for app to navigate to the correct userId

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,51 +1,52 @@
-import React from "react"
-import { useDispatch } from "react-redux"
-import { setProjects } from "redux/features/projectSlice"
-import { useNavigate } from "react-router-dom"
-import { useState } from "react"
-import projectApi from "api/projectApi"
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setProjects } from "redux/features/projectSlice";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import projectApi from "api/projectApi";
 
-import SideNav from "components/CommonUse/SideNav"
-import { Box } from "@mui/material"
-import LoadingButton from '@mui/lab/LoadingButton'
-
+import SideNav from "components/CommonUse/SideNav";
+import { Box } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 const HomePage = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
 
   const createProject = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      const res = await projectApi.create()
-      dispatch(setProjects([res]))
-      console.log(`${res.id}`)
-      navigate(`/projects/${res.id}`)
+      const res = await projectApi.create();
+      dispatch(setProjects([res]));
+      console.log(`${res.user}`);
+      navigate(`/projects/${res.user}`);
     } catch (err) {
-      alert(err)
-      console.log(err)
+      alert(err);
+      console.log(err);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   return (
-    <div className="HomePage" style={{ display: 'flex', alignItems: 'center' }}>
+    <div className="HomePage" style={{ display: "flex", alignItems: "center" }}>
       <Box>
         <SideNav />
       </Box>
-      <Box sx={{
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1
-      }}>
+      <Box
+        sx={{
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flex: 1
+        }}
+      >
         <LoadingButton
-        variant="outlined"
-        onClick={createProject}
-        loading={loading}
+          variant="outlined"
+          onClick={createProject}
+          loading={loading}
         >
           Click here to Start Your Next Collaboration
         </LoadingButton>
