7a2cc214872a29780b37696498ee8f9fa01b5fc9
xiangfengg <yongxiangfeng13@gmail.com>
2023-04-01 01:55:01 +0800

    occupation

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -22,7 +22,7 @@ export const AuthContextProvider = ({ children }) => {
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
-
+    console.log(user);
     if (user) {
       dispatch({ type: "LOGIN", payload: user });
     }
