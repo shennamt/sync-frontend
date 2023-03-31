68d1cc434e9bf04b1327cd7dcf0fef51d02de447
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-01 05:06:23 +0800

    chore: amended register page

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,107 +1,168 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import './RegisterPage.css'
+import authApi from 'api/authApi';
+
 import {
+  Box,
   TextField,
-  FormControlLabel,
-  Checkbox,
+  // FormControlLabel,
+  // Checkbox,
   Button,
-  Radio
+  // Radio
 } from '@mui/material'
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
+// import RadioGroup from '@mui/material/RadioGroup';
+import LoadingButton from '@mui/lab/LoadingButton'
+import './RegisterPage.css'
 
 const RegisterPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  // const { signup, error, isLoading } = useSignup();
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false) // checking on async action
+  const [usernameErrText, setUsernameErrText] = useState('') // store err mssg for input fields
+  const [passwordErrText, setPasswordErrText] = useState('')
+  const [confirmPasswordErrText, setConfirmPasswordErrText] = useState('')
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
+    setUsernameErrText('') // set initial state of err mssg
+    setPasswordErrText('')
+    setConfirmPasswordErrText('')
 
-    await signup(email, password);
-  };
+    const data = new FormData(e.target) // getting values from form input
+    const username = data.get('username').trim() // triming trailing whitespace
+    const password = data.get('password').trim()
+    const confirmPassword = data.get('confirmPassword').trim()
 
-  return (
-    <div className='register'>
-      <div className='register__header'>
+    let err = false
+
+    if (username === '') {
+      err = true
+      setUsernameErrText('Please fill this field')
+    }
+    if (password === '') {
+      err = true
+      setPasswordErrText('Please fill this field')
+    }
+    if (confirmPassword === '') {
+      err = true
+      setConfirmPasswordErrText('Please fill this field')
+    }
+    if (password !== confirmPassword) {
+      err = true
+      setConfirmPasswordErrText('Confirm password not match')
+    }
+
+    if (err) return
+
+    setLoading(true) // indicating submission in progress
+
+    try {
+      const res = await authApi.signup({
+        username, password, confirmPassword
+      })
+      setLoading(false)
+      localStorage.setItem('token', res.token)
+      navigate('/')
+    } catch (err) {
+      const errors = err.data.errors
+      errors.forEach(e => {
+        if (e.param === 'username') {
+          setUsernameErrText(e.msg)
+        }
+        if (e.param === 'password') {
+          setPasswordErrText(e.msg)
+        }
+        if (e.param === 'confirmPassword') {
+          setConfirmPasswordErrText(e.msg)
+        }
+      })
+      setLoading(false) // indicate form submission status
+    }
+  }
+
+    return (
+      <>
         <h1 className='sync__header'>SYNC</h1>
         <p className='body__text'>Create your account and Start Your Next Collaboration.</p>
-      </div>
 
-      <form className='register__body' onSubmit={handleSubmit}>
-        {/* <div className='input__field'>
-          <TextField label="First Name" variant="outlined" size="small" placeholder="e.g Jane" fullWidth required/>
-        </div>
-
-        <div className='input__field'>
-          <TextField label="Last Name" variant="outlined" size="small" placeholder="e.g. Doe" fullWidth required/>
-        </div> */}
-
-        <div className='input__field'>
+        <Box
+          component='form'
+          sx={{ mt: 1 }}
+          onSubmit={handleSubmit}
+          noValidate
+        >
           <TextField
-            label="Email"
-            variant="outlined"
-            size="small"
-            placeholder="e.g. xyz@gmail.com"
-            fullWidth
+            margin='normal'
             required
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
+            fullWidth
+            id='username'
+            label='Username'
+            name='username'
+            disabled={loading}
+            error={usernameErrText !== ''}
+            helperText={usernameErrText}
           />
-        </div>
-
-        <div className='input__field'>
           <TextField
-            label="Password"
-            variant="outlined"
-            size="small"
-            type="password"
-            placeholder="Enter your password"
-            fullWidth
+            margin='normal'
             required
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
+            fullWidth
+            id='password'
+            label='Password'
+            name='password'
+            type='password'
+            disabled={loading}
+            error={passwordErrText !== ''}
+            helperText={passwordErrText}
           />
-        </div>
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            id='confirmPassword'
+            label='Confirm Password'
+            name='confirmPassword'
+            type='password'
+            disabled={loading}
+            error={confirmPasswordErrText !== ''}
+            helperText={confirmPasswordErrText}
+          />
 
-        {/* <div className='input__field'>
-          <TextField label="Confirm Password" variant="outlined" size="small" type="password" placeholder="Re-enter your password" fullWidth required/>
-        </div> */}
+          {/* <div className='input__field'>
+            <FormControl className='radio__occupation'>
+              <FormLabel>Occupation</FormLabel>
+              <RadioGroup
+                defaultValue="Student"
+                aria-labelledby="demo-customized-radios"
+                name="customized-radios"
+                style = {{display: 'initial'}}
+              >
+                <FormControlLabel value="Student" control={<Radio />} label="Student" />
+                <FormControlLabel value="Professional" control={<Radio />} label="Professional" />
+              </RadioGroup>
+            </FormControl>
+          </div> */}
 
-        <div className='input__field'>
-          <FormControl className='radio__occupation'>
-            <FormLabel>Occupation</FormLabel>
-            <RadioGroup
-              defaultValue="Student"
-              aria-labelledby="demo-customized-radios"
-              name="customized-radios"
-              style = {{display: 'initial'}}
-            >
-              <FormControlLabel value="Student" control={<Radio />} label="Student" />
-              <FormControlLabel value="Professional" control={<Radio />} label="Professional" />
-            </RadioGroup>
-          </FormControl>
-        </div>
+          <LoadingButton
+            sx={{ mt: 3, mb: 2 }}
+            variant='outlined'
+            fullWidth
+            color='success'
+            type='submit'
+            loading={loading}
+          >
+            Signup
+          </LoadingButton>
+        </Box>
 
-        <FormControlLabel control={<Checkbox defaultChecked />} label="I accept the terms and conditions." />
-        <br />
-
-        <div className='button'>
-          <Button disabled={isLoading} variant="contained" fullWidth type='submit'>Login</Button>
-        </div>
-
-        {error && <div className="error">{error}</div>}
-      </form>
-
-      <div className='login__link'>
-          <p>Already have an account? <Link to="/" style={{textDecoration:'none'}}>Login</Link></p>
-      </div>
-
-    </div>
+        <Button
+          component={Link}
+          to='/login'
+          sx={{ textTransform: 'none' }}
+        >
+          Already have an account? Login
+        </Button>
+    </>
   )
 }
 
-export default RegisterPage;
+export default RegisterPage
