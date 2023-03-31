5e5f02784f957390c6df07b1c9974373f856cf26
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-01 05:17:31 +0800

    feat: provider wrap

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App"
-import { store } from "./redux/store"
-import { Provider } from 'react-redux'
-import { AuthContextProvider } from "./context/AuthContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { store } from './redux/store'
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </Provider>
-  </React.StrictMode>
-);
+  <Provider store={store}>
 
-// using Redux for state management (via Provider)
-// AuthContextProvider for managing authentication-related state
-// Wrapping AuthContextProvider inside the Provider component to make sure that both store and AuthContext are available throughout your component hierarchy.
-// Provider should wrap AuthContextProvider because the Redux store is more fundamental to the application than the authentication context.
+    <App />
+  </Provider>
+);
