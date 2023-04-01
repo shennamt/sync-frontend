393c203278bdc28abfa6cd33b63273e95db56cb1
aaronyeo007 <aaron.yeo@icloud.com>
2023-04-01 10:37:01 +0800

    chore: updated taskApo

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -2,24 +2,24 @@ import axiosClient from "./axiosClient";
 
 const taskApi = {
   // sends a POST request to create a new task with the given parameters in a
-  // specific board identified by boardId
-  create: (boardId, params) =>
-    axiosClient.post(`boards/${boardId}/tasks`, params),
+  // specific board identified by projectId
+  create: (projectId, params) =>
+    axiosClient.post(`projects/${projectId}/tasks`, params),
 
   // sends a PUT request to update the position of a task in a specific board
-  // identified by boardId, using the parameters in the params object.
-  updatePosition: (boardId, params) =>
-    axiosClient.put(`boards/${boardId}/tasks/update-position`, params),
+  // identified by projectId, using the parameters in the params object.
+  updatePosition: (projectId, params) =>
+    axiosClient.put(`projects/${projectId}/tasks/update-position`, params),
 
   // sends a DELETE request to remove a specific task with the given taskId
-  // from a specific board identified by boardId
-  delete: (boardId, taskId) =>
-    axiosClient.delete(`boards/${boardId}/tasks/${taskId}`),
+  // from a specific board identified by projectId
+  delete: (projectId, taskId) =>
+    axiosClient.delete(`projects/${projectId}/tasks/${taskId}`),
 
   // sends a PUT request to update a specific task with the given taskId in a
-  // specific board identified by boardId, using the parameters in the params object
-  update: (boardId, taskId, params) =>
-    axiosClient.put(`boards/${boardId}/tasks/${taskId}`, params)
+  // specific board identified by projectId, using the parameters in the params object
+  update: (projectId, taskId, params) =>
+    axiosClient.put(`projects/${projectId}/tasks/${taskId}`, params)
 };
 
 export default taskApi;
