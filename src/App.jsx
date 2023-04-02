7546eb349c47e94eaedfd8d251f730d23072a956
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-03 00:57:13 +0800

    feat: added assets folder

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import '@fontsource/work-sans/300.css'
+import '@fontsource/work-sans/400.css'
+import '@fontsource/work-sans/500.css'
+import '@fontsource/work-sans/700.css'
+
+function App() {
+  return (
+    <div>
+      SYNC
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+  images: {
+    logo: require('./images/logo.png'),
+    appIcon: require('./images/favicon.ico'),
+  },
+  colors: {
+    secondary: '#c2e0ff'
+  }
+}
+
+export default assets
