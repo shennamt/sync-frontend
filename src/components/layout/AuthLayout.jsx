1634c6f3e734b194c2918d7a672c4fee74c21c90
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-03 13:28:56 +0800

    feat: auth layout route

--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -1,21 +1,42 @@
 import React from 'react'
-import { Outlet, useNavigate, userLocation } from 'react-router-dom'
+import { Container, Box } from '@mui.material'
+import { useState, useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import authUtils from '../../utils/authUtils'
+import Loading from '../common/Loading'
+import assets from '../../assets'
 
 const AuthLayout = () => {
   const navigate = useNavigate()
-  const location = userLocation()
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const checkAuth = async () => {
-      //
+      const isAuth = await authUtils.isAuthenticated()
+      if (!isAuth) {
+        setLoading(false)
+      } else {
+        navigate('/')
+      }
     }
   }, [navigate])
 
   return (
-    <div>
-      <Outlet />
-    </div>
+    loading ? (
+      <Loading fullHeight/>
+    ) : (
+      <Container component='main' maxWidth='xs'>
+        <Box sx={{
+          marginTop: 8,
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column'
+        }}>
+          <img src={assets.images.logo} style={{ width: '100px' }} alt='app logo' />
+          <Outlet />
+        </Box>
+      </Container>
+    )
   )
 }
 
