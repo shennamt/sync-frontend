ad4ff0c51df50d6a6fca7bff600fbc01f08fa594
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-03 13:03:43 +0800

    chore: amending authLayout for auth utils

--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate, userLocation } from 'react-router-dom'
 
 const AuthLayout = () => {
+  const navigate = useNavigate()
+  const location = userLocation()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      //
+    }
+  }, [navigate])
+
   return (
     <div>
       <Outlet />
