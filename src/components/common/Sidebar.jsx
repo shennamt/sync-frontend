2343c6ebc7d4f26fd5e19a06d42322279f62f6db
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-04 07:45:32 +0800

    feat: board updating, dnd, and boardpage render

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,18 +1,21 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import {
   Box,
   Drawer,
   IconButton,
+  Link,
   List,
   ListItem,
+  ListItemButton,
   Typography
 } from "@mui/material";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import assets from "../../assets/index";
 import { setBoards } from "../../redux/features/boardSlice"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import boardApi from "../../api/boardApi"
 
 const Sidebar = () => {
@@ -20,6 +23,8 @@ const Sidebar = () => {
   const boards = useSelector((state) => state.board.value);
   const navigate = useNavigate();
   const dispatch = useDispatch(); // dispatch actions to update state
+  const { boardId } = useParams()
+  const [activeIndex, setActiveIndex] = useState(0) // arr destructuring to create 2 new vars
   const sidebarWidth = 250;
 
   useEffect(() => { // fetch data from API and update redux store state
@@ -32,22 +37,25 @@ const Sidebar = () => {
       }
     }
     getBoards()
-  }, []) // empty array passed as second argument means function will only be called once aft component has rendered
+  }, [dispatch])
 
   useEffect(() => {
-    console.log(boards)
-  }, [boards])
+    const activeItem = boards.findIndex(e => e._id === boardId) // looks for board
+    if (boards.length > 0 && boardId === undefined) {
+      navigate(`/boards/${boards[0]._id}`) // nav to the board id clicked or first board depending on arr
+    }
+    setActiveIndex(activeItem) // update state of active board
+  }, [boards, boardId, navigate])
 
   const logout = () => {
     localStorage.removeItem("token");
     navigate("/login");
   };
 
-  // variant=`permanent` sets drawer as always visible on the screen
-  // open=`true` sets the initial state of the drawer to open
-  // & > div selector targets all immediate child div elements of the
-  // current element and sets their border-right property to none.
-  // `div` element is a direct child of the Drawer component.
+  const onDragEnd = () => {
+
+  }
+
   return (
     <Drawer
       container={window.document.body}
@@ -57,16 +65,14 @@ const Sidebar = () => {
         width: sidebarWidth,
         height: "100%",
         "& > div": { borderRight: "none" }
-      }}
-    >
+      }}>
       <List
         disablePadding
         sx={{
           width: sidebarWidth,
           height: "100vh",
           backgroundColor: assets.colors.secondary
-        }}
-      >
+        }}>
         <ListItem>
           <Box
             sx={{
@@ -107,8 +113,7 @@ const Sidebar = () => {
               display: "flex",
               alignItems: "center",
               justifyContent: "space-between"
-            }}
-          >
+            }}>
             <Typography variant="body2" fontWeight="700" color="white">
               Private
             </Typography>
@@ -117,6 +122,44 @@ const Sidebar = () => {
             </IconButton>
           </Box>
         </ListItem>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable key={'list-board-droppable'} droppableId={'list-board-droppable'}>
+            {(provided) => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {
+                  boards.map((item, index) => (
+                    <Draggable key={item._id} draggableId={item._id} index={index}>
+                      {(provided, snapshot) => (
+                        <ListItemButton
+                          ref={provided.innerRef}
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}
+                          selected={index === activeIndex}
+                          component={Link}
+                          to={`/boards/${item._id}`}
+                          sx={{
+                            pl: '20px',
+                            cursor:snapshot.isDragging ? 'grab' : 'pointer!important'
+                          }}
+                          >
+                            <Typography
+                              variant='body2'
+                              fontWeight='700'
+                              color='white'
+                              sx={{ whitespace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+
+                            >
+                              {item.title}
+                            </Typography>
+                        </ListItemButton>
+                      )}
+                    </Draggable>
+                  ))
+                }
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </List>
     </Drawer>
   );
