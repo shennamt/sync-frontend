9c2f35fa39a52ddf884130126fd3f2424950df3d
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-03 15:23:45 +0800

    feat: signup page made. but there's a verify token error brb

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import authApi from '../api/authApi'
+
 
 import { Box, TextField, Button, Typography } from '@mui/material'
 import LoadingButton from '@mui/lab/LoadingButton'
@@ -29,22 +31,49 @@ const SignupPage = () => {
 
     if (username === '') {
       err = true
-      setUsernameErrText("Please provide a username")
+      setUsernameErrText('Please fill this field')
     }
     if (password === '') {
       err = true
-      setPasswordErrText("Please provide a password")
+      setPasswordErrText('Please fill this field')
     }
     if (confirmPassword === '') {
       err = true
-      setConfirmPasswordErrText("Please fill in this field")
+      setConfirmPasswordErrText('Please fill this field')
     }
     if (password !== confirmPassword) {
       err = true
-      setConfirmPasswordErrText("Passwords do not match")
+      setConfirmPasswordErrText('Confirm password not match')
     }
+
     if (err) return
+
+    setLoading(true)
+
+    try {
+      const res = await authApi.signup({
+        username, password, confirmPassword
+      })
+      setLoading(false)
+      localStorage.setItem('token', res.token)
+      navigate('/')
+    } catch (err) {
+      const errors = err.data.errors
+      errors.forEach(e => {
+        if (e.param === 'username') {
+          setUsernameErrText(e.msg)
+        }
+        if (e.param === 'password') {
+          setPasswordErrText(e.msg)
+        }
+        if (e.param === 'confirmPassword') {
+          setConfirmPasswordErrText(e.msg)
+        }
+      })
+      setLoading(false)
+    }
   }
+
   return (
     <>
       <Box>
