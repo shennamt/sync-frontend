be5e3cc72376593dd385191eb978904a1a056072
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-04 09:11:14 +0800

    feat: board.jsx working

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import boardApi from '../api/boardApi'
 
 const Board = () => {
-  return <div>Board</div>;
+  const { boardId } = useParams()
+  console.log(boardId)
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [sections, setSections] = useState([])
+  const [isFavourite, setIsFavourite] = useState(false)
+  const [icon, setIcon] = useState('')
+
+  useEffect(() => {
+    const getBoard = async () => {
+      try {
+        const res = await boardApi.getOne(boardId)
+        setTitle(res.title)
+        setDescription(res.description)
+        setSections(res.sections)
+        setIsFavourite(res.favourite)
+        setIcon(res.icon)
+      } catch (err) {
+        alert(err)
+      }
+    }
+    getBoard()
+  }, [boardId])
+
+  return <div>
+    Board
+  </div>;
 };
 
 export default Board;
