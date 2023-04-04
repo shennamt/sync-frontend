8ec056c7bbe09d5447a5260accf65cf1c3c10995
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-04 14:39:07 +0800

    front end flesh

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,19 +1,29 @@
-import React from "react";
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { Box, IconButton, TextField } from '@mui/material'
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined'
 import StarOutlinedIcon from '@mui/icons-material/StarOutlined'
+import { Box, IconButton, TextField } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
 import boardApi from '../api/boardApi'
+import Kanban from '../components/common/Kanban'
+import { setBoards } from '../redux/features/boardSlice'
+import { setFavouriteList } from '../redux/features/favouriteSlice'
 
+let timer
+const timeout = 500
 
 const Board = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const { boardId } = useParams()
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
   const [sections, setSections] = useState([])
   const [isFavourite, setIsFavourite] = useState(false)
+
+  const boards = useSelector((state) => state.board.value)
+  const favouriteList = useSelector((state) => state.favourites.value)
 
   useEffect(() => {
     const getBoard = async () => {
@@ -23,24 +33,119 @@ const Board = () => {
         setDescription(res.description)
         setSections(res.sections)
         setIsFavourite(res.favourite)
-        console.log(res)
       } catch (err) {
         alert(err)
-        console.log(err)
       }
     }
     getBoard()
   }, [boardId])
 
+  const onIconChange = async (newIcon) => {
+    let temp = [...boards]
+    const index = temp.findIndex(e => e.id === boardId)
+    temp[index] = { ...temp[index], icon: newIcon }
+
+    if (isFavourite) {
+      let tempFavourite = [...favouriteList]
+      const favouriteIndex = tempFavourite.findIndex(e => e.id === boardId)
+      tempFavourite[favouriteIndex] = { ...tempFavourite[favouriteIndex], icon: newIcon }
+      dispatch(setFavouriteList(tempFavourite))
+    }
+
+    setIcon(newIcon)
+    dispatch(setBoards(temp))
+    try {
+      await boardApi.update(boardId, { icon: newIcon })
+    } catch (err) {
+      alert(err)
+    }
+  }
+
+  const updateTitle = async (e) => {
+    clearTimeout(timer)
+    const newTitle = e.target.value
+    setTitle(newTitle)
+
+    let temp = [...boards]
+    const index = temp.findIndex(e => e.id === boardId)
+    temp[index] = { ...temp[index], title: newTitle }
+
+    if (isFavourite) {
+      let tempFavourite = [...favouriteList]
+      const favouriteIndex = tempFavourite.findIndex(e => e.id === boardId)
+      tempFavourite[favouriteIndex] = { ...tempFavourite[favouriteIndex], title: newTitle }
+      dispatch(setFavouriteList(tempFavourite))
+    }
+
+    dispatch(setBoards(temp))
+
+    timer = setTimeout(async () => {
+      try {
+        await boardApi.update(boardId, { title: newTitle })
+      } catch (err) {
+        alert(err)
+      }
+    }, timeout);
+  }
+
+  const updateDescription = async (e) => {
+    clearTimeout(timer)
+    const newDescription = e.target.value
+    setDescription(newDescription)
+    timer = setTimeout(async () => {
+      try {
+        await boardApi.update(boardId, { description: newDescription })
+      } catch (err) {
+        alert(err)
+      }
+    }, timeout);
+  }
+
+  const addFavourite = async () => {
+    try {
+      const board = await boardApi.update(boardId, { favourite: !isFavourite })
+      let newFavouriteList = [...favouriteList]
+      if (isFavourite) {
+        newFavouriteList = newFavouriteList.filter(e => e.id !== boardId)
+      } else {
+        newFavouriteList.unshift(board)
+      }
+      dispatch(setFavouriteList(newFavouriteList))
+      setIsFavourite(!isFavourite)
+    } catch (err) {
+      alert(err)
+    }
+  }
+
+  const deleteBoard = async () => {
+    try {
+      await boardApi.delete(boardId)
+      if (isFavourite) {
+        const newFavouriteList = favouriteList.filter(e => e.id !== boardId)
+        dispatch(setFavouriteList(newFavouriteList))
+      }
+
+      const newList = boards.filter(e => e.id !== boardId)
+      if (newList.length === 0) {
+        navigate('/boards')
+      } else {
+        navigate(`/boards/${newList[0].id}`)
+      }
+      dispatch(setBoards(newList))
+    } catch (err) {
+      alert(err)
+    }
+  }
+
   return (
     <>
-      <Box x={{
+      <Box sx={{
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'space-between',
         width: '100%'
       }}>
-        <IconButton variant='outlined'>
+        <IconButton variant='outlined' onClick={addFavourite}>
           {
             isFavourite ? (
               <StarOutlinedIcon color='warning' />
@@ -49,14 +154,15 @@ const Board = () => {
             )
           }
         </IconButton>
-        <IconButton variant='outlined' color='error'>
+        <IconButton variant='outlined' color='error' onClick={deleteBoard}>
           <DeleteOutlinedIcon />
-        </IconButton> 
+        </IconButton>
       </Box>
       <Box sx={{ padding: '10px 50px' }}>
         <Box>
           <TextField
             value={title}
+            onChange={updateTitle}
             placeholder='Untitled'
             variant='outlined'
             fullWidth
@@ -68,6 +174,7 @@ const Board = () => {
           />
           <TextField
             value={description}
+            onChange={updateDescription}
             placeholder='Add a description'
             variant='outlined'
             multiline
@@ -79,12 +186,13 @@ const Board = () => {
             }}
           />
         </Box>
-        {/* <Box>
-          <Kanban data={sections} boardId={boardId} />
-        </Box> */}
+        <Box>
+          {/* Kanban board */}
+          {/* <Kanban data={sections} boardId={boardId} /> */}
+        </Box>
       </Box>
     </>
   )
-};
+}
 
-export default Board;
+export default Board
