cfe92f9714f53fa5d3fad09e6718d7bad77904b1
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-04 14:31:49 +0800

    feat: board

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Box, IconButton, TextField } from '@mui/material'
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined'
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined'
 import boardApi from '../api/boardApi'
+
 
 const Board = () => {
   const { boardId } = useParams()
@@ -27,9 +32,59 @@ const Board = () => {
     getBoard()
   }, [boardId])
 
-  return <div>
-    Board
-  </div>;
+  return (
+    <>
+      <Box x={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%'
+      }}>
+        <IconButton variant='outlined'>
+          {
+            isFavourite ? (
+              <StarOutlinedIcon color='warning' />
+            ) : (
+              <StarBorderOutlinedIcon />
+            )
+          }
+        </IconButton>
+        <IconButton variant='outlined' color='error'>
+          <DeleteOutlinedIcon />
+        </IconButton> 
+      </Box>
+      <Box sx={{ padding: '10px 50px' }}>
+        <Box>
+          <TextField
+            value={title}
+            placeholder='Untitled'
+            variant='outlined'
+            fullWidth
+            sx={{
+              '& .MuiOutlinedInput-input': { padding: 0 },
+              '& .MuiOutlinedInput-notchedOutline': { border: 'unset ' },
+              '& .MuiOutlinedInput-root': { fontSize: '2rem', fontWeight: '700' }
+            }}
+          />
+          <TextField
+            value={description}
+            placeholder='Add a description'
+            variant='outlined'
+            multiline
+            fullWidth
+            sx={{
+              '& .MuiOutlinedInput-input': { padding: 0 },
+              '& .MuiOutlinedInput-notchedOutline': { border: 'unset ' },
+              '& .MuiOutlinedInput-root': { fontSize: '0.8rem' }
+            }}
+          />
+        </Box>
+        {/* <Box>
+          <Kanban data={sections} boardId={boardId} />
+        </Box> */}
+      </Box>
+    </>
+  )
 };
 
 export default Board;
