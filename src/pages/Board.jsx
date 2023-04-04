7b1877ad1080fb6d3da6925fd8c798cfe74a2079
aaronyeo007 <aaron.yeo@icloud.com>
2023-04-04 14:31:13 +0800

    chore: - removed React import - some updates due to prettier

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,35 +1,32 @@
-import React from "react";
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import boardApi from '../api/boardApi'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import boardApi from "../api/boardApi";
 
 const Board = () => {
-  const { boardId } = useParams()
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [sections, setSections] = useState([])
-  const [isFavourite, setIsFavourite] = useState(false)
+  const { boardId } = useParams();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [sections, setSections] = useState([]);
+  const [isFavourite, setIsFavourite] = useState(false);
 
   useEffect(() => {
     const getBoard = async () => {
       try {
-        const res = await boardApi.getOne(boardId)
-        setTitle(res.title)
-        setDescription(res.description)
-        setSections(res.sections)
-        setIsFavourite(res.favourite)
-        console.log(res)
+        const res = await boardApi.getOne(boardId);
+        setTitle(res.title);
+        setDescription(res.description);
+        setSections(res.sections);
+        setIsFavourite(res.favourite);
+        console.log(res);
       } catch (err) {
-        alert(err)
-        console.log(err)
+        alert(err);
+        console.log(err);
       }
-    }
-    getBoard()
-  }, [boardId])
+    };
+    getBoard();
+  }, [boardId]);
 
-  return <div>
-    Board
-  </div>;
+  return <div>Board</div>;
 };
 
 export default Board;
